13e92360e27755e23b99fe472407dae8d566cff3
Mr. Cetto <contact@phoenixthrush.com>
2026-01-09 16:16:08 +0100

    add constants

--- templates/fetch.py
+++ templates/fetch.py
@@ -1,18 +1,10 @@
 import re
-import sys
 import json
 import os
 import urllib.request
 from html import unescape
 
-SITES = {
-    "1-3": "https://itexamanswers.net/ccna-1-v7-modules-1-3-basic-network-connectivity-and-communications-exam-answers.html",
-    "4-7": "https://itexamanswers.net/ccna-1-v7-modules-4-7-ethernet-concepts-exam-answers.html",
-    "8-10": "https://itexamanswers.net/ccna-1-v7-modules-8-10-communicating-between-networks-exam-answers.html",
-    "11-13": "https://itexamanswers.net/ccna-1-v7-modules-11-13-ip-addressing-exam-answers-full.html",
-    "14-15": "https://itexamanswers.net/ccna-1-v7-modules-14-15-network-application-communications-exam-answers.html",
-    "16-17": "https://itexamanswers.net/ccna-1-v7-modules-16-17-building-and-securing-a-small-network-exam-answers.html",
-}
+from constants import SITES
 
 # -----------------------------
 # Precompiled regex patterns
